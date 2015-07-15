846dd551b3951dc50e6520075538b9c6b1283b8a
empey <empey@empeydeMac-mini.local>
2015-07-15 17:13:53 +0800

    Initial Commit

---
+++ wepapp/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  wepapp
+//
+//  Created by empey on 15/7/15.
+//  Copyright (c) 2015年 empey. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ wepapp/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  wepapp
+//
+//  Created by empey on 15/7/15.
+//  Copyright (c) 2015年 empey. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
